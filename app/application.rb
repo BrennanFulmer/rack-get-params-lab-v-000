63f9383ea9af0d9362b403187a4882670f8a48b0
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-05 19:14:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,6 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/items/)
       if @@cart.empty?
+        
       else
         @@cart.each do |my_item|
           resp.write "#{my_item}\n"
